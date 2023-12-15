c9943c9e556faab0077e806d2d90924f2d3186bb
Roman <romchikus1987@gmail.com>
2023-12-15 19:02:17 +0200

    updated

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,14 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+  video: false,
+  chromeWebSecurity: false,
+  modifyObstructiveCode: false,
+  viewportWidth: 1920,
+  viewportHeight: 1080,
+  env: {
+    CI: 'true'
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
