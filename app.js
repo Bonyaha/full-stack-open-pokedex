228d0b8e83eba43669d2622cca2cb77c8fe7dd6b
Roman <romchikus1987@gmail.com>
2023-12-20 15:53:03 +0200

    final helth check

--- app.js
+++ app.js
@@ -7,9 +7,7 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 app.get('/health', (req, res) => {
-  throw 'error...'
-  // eslint-disable-next-line no-unreachable
-  res.send('ok')
+  res.send('Ok')
 })
 app.listen(PORT, () => {
   console.log('server started on port 5000')
