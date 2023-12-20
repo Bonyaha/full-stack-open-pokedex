6ef7f4c43e57fa3079e252beb4bcacfc36b405b4
Roman <romchikus1987@gmail.com>
2023-12-20 13:14:33 +0200

    updated

--- app.js
+++ app.js
@@ -6,9 +6,7 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
-app.get('/version', (req, res) => {
-  res.send('2') // change this string to ensure a new version deployed
-})
+
 app.listen(PORT, () => {
   console.log('server started on port 5000')
 })
